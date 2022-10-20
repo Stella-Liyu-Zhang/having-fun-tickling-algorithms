d06715d572121e9f6e13353a6349e1dd4943b2c3
Stella <l9zhang@ucsd.edu>
2022-10-19 18:17:43 -0700

    21

---
+++ LinkedList/21. Merge Two Sorted Lists.java
@@ -0,0 +1,48 @@
+//Iterative method
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null)
+            return list2;
+        else if (list2 == null)
+            return list1;
+
+        ListNode dummy = new ListNode(0);
+        ListNode pointer = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                pointer.next = list1;
+                list1 = list1.next;
+            } else {
+                pointer.next = list2;
+                list2 = list2.next;
+            }
+            pointer = pointer.next;
+        }
+        if (list1 != null) {
+            pointer.next = list1;
+        } else {
+            pointer.next = list2;
+        }
+        return dummy.next;
+    }
+}
+
+// Recursive method
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
