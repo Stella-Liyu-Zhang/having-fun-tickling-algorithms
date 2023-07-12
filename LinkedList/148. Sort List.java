54353f6986fe7382b2a619dcb33a57b71908e8b4
Stella <l9zhang@ucsd.edu>
2023-07-12 11:14:07 -0700

    148

---
+++ LinkedList/148. Sort List.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode left = head;
+        ListNode right = getMid(head);
+        ListNode temp = right.next;
+        right.next = null;
+        right = temp;
+
+        left = sortList(left);
+        right = sortList(right);
+        return merge(left, right);
+    }
+
+    private ListNode getMid(ListNode head) {
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        // both left and right are sorted;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        // Compare the values of the nodes in l1 and l2 and add the smaller value to the
+        // merged list
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                curr.next = left;
+                left = left.next;
+            } else {
+                curr.next = right;
+                right = right.next;
+            }
+            curr = curr.next;
+        }
+        // Add the remaining nodes of l1 or l2 to the merged list
+        if (left != null) {
+            curr.next = left;
+        }
+        if (right != null) {
+            curr.next = right;
+        }
+        return dummy.next;
+    }
+}
