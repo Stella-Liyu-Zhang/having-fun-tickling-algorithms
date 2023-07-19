9351731350b4d5d9a250817bb97af838bcdad11f
Stella <l9zhang@ucsd.edu>
2023-07-19 10:28:56 -0700

    322

--- Dynamic Programming/322. Coin Change.java
+++ Dynamic Programming/322. Coin Change.java
@@ -7,16 +7,12 @@ class Solution {
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
-        for (int i = 0; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
-                } else {
-                    break;
-                }
+
+        for(int coin: coins){
+            for(int i = coin; i <= amount; i ++){
+                dp[i] = Math.min(dp[i], dp[i-coin] + 1);
             }
         }
-
         return dp[amount] > amount ? -1 : dp[amount];
     }
 }
