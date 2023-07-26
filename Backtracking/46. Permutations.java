920491ada8101bc3924d46465506a20057920163
Stella <l9zhang@ucsd.edu>
2023-07-25 23:58:51 -0700

    46

--- Backtracking/46. Permutations.java
+++ Backtracking/46. Permutations.java
@@ -13,6 +13,13 @@ class Solution {
      * If we exhausted the current branch, currResult.size() == nums.length, we will
      * backtrack.
      * To make sure each element is used once, we establish boolean[] used.
+
+        So, while it seems weird to remove elements from the temp array after getting [1,2,3]
+         (which leaves you with [1,2]), it starts to make a lot of sense when you go back   
+         another level on the recursion and reduce [1,2] to [1], and now you are able to make it [1,3] with your for loop, allowing for full permutation.
+
+         this is the gist of backtracking. Restore state and let upper level of recursion to continue 
+         visit this element。
      */
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
