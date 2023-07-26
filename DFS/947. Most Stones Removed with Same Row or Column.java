5a45673b4a8995af677f8ba1910dd6d69e9239b7
Stella <l9zhang@ucsd.edu>
2023-07-26 11:05:23 -0700

    947

---
+++ DFS/947. Most Stones Removed with Same Row or Column.java
@@ -0,0 +1,27 @@
+class Solution {
+    // Time : O(N^2), N = # of stones
+    // Space: O(N)
+    public int removeStones(int[][] stones) {
+        Set<int[]> visited = new HashSet<>();
+        int numOfIslands = 0;
+        for(int[] s1: stones){
+            if (!visited.contains(s1)){
+                dfs(s1, visited, stones);
+                numOfIslands ++;
+            }
+        }
+        return stones.length - numOfIslands;
+    }
+
+    private void dfs(int[] s1, Set<int[]> visited, int[][] stones){
+        visited.add(s1);
+        for(int[] s2: stones){
+            if(!visited.contains(s2)){
+                //stone with same row or column group them into island
+                if(s1[0] == s2[0] || s1[1] == s2[1]){
+                    dfs(s2, visited, stones);
+                }
+            }
+        }
+    }
+}
