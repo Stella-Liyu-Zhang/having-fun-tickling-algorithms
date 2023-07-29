9ece9f589a87f82dc37e10442dc0cdea7f58138f
Stella <l9zhang@ucsd.edu>
2023-07-28 21:19:26 -0700

    24 recursive solution

--- LinkedList/24. Swap Nodes in Pairs.java
+++ LinkedList/24. Swap Nodes in Pairs.java
@@ -32,3 +32,20 @@ class Solution {
         return dummy.next;
     }
 }
+
+
+/*
+* Recursive stack uses O(n) space
+* 
+*/
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
+}
