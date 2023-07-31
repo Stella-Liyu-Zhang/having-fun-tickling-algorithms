431fe92882215093a25b1d1541bb7bc469f46414
Stella <l9zhang@ucsd.edu>
2023-07-31 16:35:30 -0700

    20

--- Stack/20. Valid Parentheses.java
+++ Stack/20. Valid Parentheses.java
@@ -1,18 +1,24 @@
 class Solution {
     // stack: first in last out
+class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<Character>();
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
+        /*
+            {[()]}
+        */
+        Stack<Character> stack = new Stack();
+        for(char c: s.toCharArray()){
+            if (c == '('){
                 stack.push(')');
-            } else if (c == '{') {
-                stack.push('}');
-            } else if (c == '[') {
+            }else if (c == '['){
                 stack.push(']');
-            } else if (stack.isEmpty() || stack.pop() != c) {
+            }else if (c == '{'){
+                stack.push('}');
+            }else if (stack.isEmpty() || stack.pop() != c) {
                 return false;
             }
         }
         return stack.isEmpty();
+
     }
+}
 }
