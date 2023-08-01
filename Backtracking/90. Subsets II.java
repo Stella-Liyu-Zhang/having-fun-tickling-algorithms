fa3a10c4baf4ca1a7bac15da5b4d747911f2de52
Stella <l9zhang@ucsd.edu>
2023-08-01 14:39:51 -0700

    90

--- Backtracking/90. Subsets II.java
+++ Backtracking/90. Subsets II.java
@@ -1,15 +1,26 @@
-public List<List<Integer>> subsetsWithDup(int[] nums) {
-    List<List<Integer>> list = new ArrayList();
-    Arrays.sort(nums);
-    backtrack(list, nums, new ArrayList<>(), 0);
-    return list;
-}
-public void backtrack(List<List<Integer>> list, int[] nums, List<Integer> temp, int start){
-    list.add(new ArrayList<>(temp));
-    for(int i = start; i < nums.length; i ++){
-        if (i > start && nums[i] == nums[i-1]) continue;
-        temp.add(nums[i]);
-        backtrack(list, nums, temp, i + 1);
-        temp.remove(temp.size() - 1);
+class Solution {
+    /*
+    Find the power set (all possible subsets) excluding duplicate subsets. a total of 2^n distinct subsets for an array of length n with no duplicates.
+
+    */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new  ArrayList<>();
+        subsetHelper(res, new ArrayList<>(), nums, 0);
+        return res;
+    }
+    //generate all the susets
+    private void subsetHelper(List<List<Integer>> subsets, List<Integer> curr, int[] nums, int start){
+        subsets.add(new ArrayList<>(curr));
+
+        for(int i = start; i < nums.length; i ++){
+            //If the current element is a duplicate, ignore
+            if (i != start && nums[i] == nums[i-1]){
+                continue;
+            }
+            curr.add(nums[i]);
+            subsetHelper(subsets, curr, nums, i + 1);
+            curr.remove(curr.size() - 1);
+        }
     }
 }
