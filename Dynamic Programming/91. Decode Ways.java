7a3fe36de01b71c2e0c3475efbab1a7da3087c86
Stella <l9zhang@ucsd.edu>
2023-08-17 18:57:42 -0700

    91

--- Dynamic Programming/91. Decode Ways.java
+++ Dynamic Programming/91. Decode Ways.java
@@ -19,3 +19,30 @@ class Solution {
         return dp[s.length()];
     }
 }
+
+//Second attempt
+class Solution {
+    /*
+    bottom up processing
+    use all the sub-problems up to n to reach.
+
+    dp[n] stores the maximum number of ways to decode the string up to n. 
+    */
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0:1;
+        for(int i = 2; i <= s.length(); i ++){
+            int oneDigit = Integer.valueOf(s.substring(i-1, i));
+            int twoDigit = Integer.valueOf(s.substring(i-2, i));
+            if(oneDigit > 0 && oneDigit <= 9){
+                dp[i] += dp[i-1];
+            }
+            //don't need an else statement, because we would add twice if both works
+            if(twoDigit >= 10 && twoDigit <= 26){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[s.length()];
+    }
+}
