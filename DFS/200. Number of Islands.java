016e94a6616a60ce42b7dde830effb09c1a0c60d
Stella <l9zhang@ucsd.edu>
2023-08-17 17:16:12 -0700

    200

--- DFS/200. Number of Islands.java
+++ DFS/200. Number of Islands.java
@@ -28,3 +28,37 @@ class Solution {
     }
 
 }
+
+//second attempt
+class Solution {
+    /*
+    need to traverse an entire matrix
+    */
+    public int numIslands(char[][] grid) {
+        if(grid.length == 0 || grid == null) return 0;
+        int numIslands = 0;
+        
+        for(int i = 0; i < grid.length; i ++){
+            for(int j = 0; j < grid[0].length; j ++){
+                if(grid[i][j] == '1'){
+                    numIslands += dfs(grid, i, j);
+                }
+            }
+        }
+        return numIslands;
+    }
+    private int dfs(char[][] grid, int i, int j){
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0'){
+            return 0;
+        }
+        //sink the current
+        grid[i][j] = '0';
+        //sink the neighbours
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+        //found an island
+        return 1;
+    }
+}
