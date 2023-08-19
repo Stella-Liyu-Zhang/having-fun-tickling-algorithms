b9dfaf927756af70001eaf7e4787929992c1032d
Stella <l9zhang@ucsd.edu>
2023-08-19 09:51:41 -0700

    349

--- Arrays and ArrayList/349. Intersection of Two Arrays.java
+++ Arrays and ArrayList/349. Intersection of Two Arrays.java
@@ -19,3 +19,38 @@ class Solution {
         return output;
     }
 }
+
+// binary search solution
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        //Binary search
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for(int num: nums1){
+            if(binarySearch(nums2, num)){
+                set.add(num);
+            }
+        }
+        int[] ans = new int[set.size()];
+        int i = 0;
+        for(Integer num: set){
+            ans[i++] = num;
+        }
+        return ans;
+    }
+    private boolean binarySearch(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target){
+                return true;
+            }else if (nums[mid] > target){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
