b963284dc4f4de38a25e2f16a5dd9f3c52cc378d
Stella <l9zhang@ucsd.edu>
2023-08-23 10:10:49 -0700

    345

--- Two Pointers/345. Reverse Vowels of a String.java
+++ Two Pointers/345. Reverse Vowels of a String.java
@@ -1,32 +1,31 @@
 class Solution {
+    /*
+    Time: O(n)
+    Space: O(n)
+    */
     public String reverseVowels(String s) {
-        char[] sArray = s.toCharArray();
-        int left = 0, right = sArray.length - 1;
-
-        while (left < right) {
-            if (isVowel(sArray[left]) && isVowel(sArray[right])) {
-                swap(sArray, left, right);
-                left++;
-                right--;
-            }
-            if (!isVowel(sArray[left])) {
-                left++;
-            }
-            if (!isVowel(sArray[right])) {
-                right--;
+        char[] arr = s.toCharArray();
+        int left = 0, right = s.length() - 1;
+        while(left < right){
+            if(isVowel(arr[left]) && isVowel(arr[right])){
+                swap(arr, left, right);
+                left ++;
+                right --;
+            }else if (!isVowel(arr[left])){
+                left ++;
+            }else if (!isVowel(arr[right])){
+                right --; 
             }
         }
-        return new String(sArray);
+        return new String(arr);
     }
-
-    public void swap(char[] arr, int i, int j) {
+    private void swap (char[] arr, int i, int j){
         char temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-
-    public boolean isVowel(char ch) {
-        char c = Character.toLowerCase(ch);
+    private boolean isVowel (char c){
+        c = Character.toLowerCase(c);
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 }
