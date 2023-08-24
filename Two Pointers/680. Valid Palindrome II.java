22e3d59a42d4b053d7a4a44879808da9c7da33cd
Stella <l9zhang@ucsd.edu>
2023-08-24 09:03:26 -0700

    680

--- Two Pointers/680. Valid Palindrome II.java
+++ Two Pointers/680. Valid Palindrome II.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int i = 0, j = s.length() - 1;
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
-            }
-            i++;
-            j--;
+        int left = 0; 
+        int right = s.length() - 1;
+        while(left < right){
+           if(s.charAt(left) != s.charAt(right)){
+               return isPalindrome(s, left+1, right) || isPalindrome(s, left, right - 1);
+           }
+           left ++;
+           right --;
         }
         return true;
     }
-
-    private boolean isPalindrome(String s, int i, int j) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
+    private boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
-            i++;
-            j--;
+            left ++;
+            right --;
         }
         return true;
     }
