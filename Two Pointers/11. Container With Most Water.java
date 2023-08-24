6206532ff8fb4ebf307b48e88ba3d23eae992e54
Stella <l9zhang@ucsd.edu>
2023-08-24 14:31:54 -0700

    11

--- Two Pointers/11. Container With Most Water.java
+++ Two Pointers/11. Container With Most Water.java
@@ -1,19 +1,15 @@
 class Solution {
     public int maxArea(int[] height) {
-        // Between 2 walls, the smaller height[] is the height of the box. and the area
-        // is 1*(min(height[a], height[b]))
-        // Between n walls, the smaller height is the (n-1)*(min(height[0],
-        // height[n-1]));
-
-        int maxArea = Integer.MIN_VALUE;
         int left = 0, right = height.length - 1;
-        while (left < right) {
-            int currArea = (right - left) * (Math.min(height[right], height[left]));
-            maxArea = Math.max(maxArea, currArea);
-            if (height[left] < height[right])
-                left++;
-            else
-                right--;
+        int maxArea = 0;
+        while(left < right){
+            int currArea = Math.min(height[right], height[left])*(right - left);
+            maxArea = Math.max(currArea, maxArea);
+            if(height[left] <= height[right]){
+                left ++;
+            }else{
+                right --;
+            }
         }
         return maxArea;
     }
