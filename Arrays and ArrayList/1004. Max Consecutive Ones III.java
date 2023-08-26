365092f31109454482a4e1b01d12d961b96c0330
Stella <l9zhang@ucsd.edu>
2023-08-25 21:42:34 -0700

    1004

--- Arrays and ArrayList/1004. Max Consecutive Ones III.java
+++ Arrays and ArrayList/1004. Max Consecutive Ones III.java
@@ -1,18 +1,23 @@
 class Solution {
     /*
-    keep track of slow pointer, pointing to the left
-    return the final max length
+    If we included a zero in the window we reduce the value of k.
+    Since k is the maximum zeros allowed in a window.
 
+    A negative k denotes we have consumed all allowed flips and window has
+    more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
+
+    slow
+    fast
 
     */
     public int longestOnes(int[] nums, int k) {
         int slow = 0, fast = 0;
-        while (fast < nums.length){
+        while(fast < nums.length){
             if(nums[fast] == 0){
-                k--;
+                k --;
             }
             if(k < 0){
-                if(nums[slow] == 0){
+                if(nums[slow] == 0){// If the left element to be thrown out is zero we increase k.
                     k ++;
                 }
                 slow ++;
