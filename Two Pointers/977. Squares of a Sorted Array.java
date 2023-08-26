f2e7189248573167072cd4c774dbe7c6b24670d4
Stella <l9zhang@ucsd.edu>
2023-08-25 23:38:54 -0700

    977

---
+++ Two Pointers/977. Squares of a Sorted Array.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int[] sortedSquares(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        int[] ans = new int[nums.length];
+
+        for(int i = nums.length - 1; i >= 0; i --){
+            if(Math.abs(nums[left]) < Math.abs(nums[right])){
+                ans[i] = nums[right]*nums[right];
+                right --;
+            }else{
+                ans[i] = nums[left]*nums[left];
+                left ++;
+            }
+        }
+        return ans;
+    }
+}
