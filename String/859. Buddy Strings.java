01a9b7ee2bd9c7e4a8c583c7925ecea36725aa59
Stella <l9zhang@ucsd.edu>
2023-09-10 12:08:15 -0700

    859

--- String/859. Buddy Strings.java
+++ String/859. Buddy Strings.java
@@ -42,3 +42,36 @@ class Solution {
         }
     }
 }
+
+//second attempt
+class Solution {
+    /*
+    3 scenarios:
+    1. s and goal have diff length
+    2. the two strings are equal, so we swap the same nummber
+    3. the 2 strings have 2 diffs, and they are the same diction of 2 letters
+
+
+    */
+    public boolean buddyStrings(String s, String goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+        if(s.equals(goal)){
+            Set<Character> set = new HashSet<>();
+            for(char c: s.toCharArray()){
+                set.add(c);
+            }
+            return set.size() < s.length();
+        }
+        List<Integer> diff = new ArrayList<>();
+        for(int i = 0; i < s.length(); i ++){
+            if(s.charAt(i) != goal.charAt(i)){
+                diff.add(i);
+            }
+        }
+        return (diff.size() == 2 
+                && s.charAt(diff.get(0)) == goal.charAt(diff.get(1)) 
+                && s.charAt(diff.get(1)) == goal.charAt(diff.get(0)));
+    }
+}
