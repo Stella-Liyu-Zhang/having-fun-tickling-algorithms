657cea6acc537c76b286c36d068eb40b66ea12fb
Stella <l9zhang@ucsd.edu>
2023-09-11 21:52:22 -0700

    268

--- Bit manipulation/268. Missing Number.java
+++ Bit manipulation/268. Missing Number.java
@@ -16,3 +16,14 @@ class Solution {
         return res;
     }
 }
+
+//Solution 2
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = (1+nums.length)*nums.length/2;
+        for(int i : nums){
+            sum -= i;
+        }
+        return sum;
+    }
+}
