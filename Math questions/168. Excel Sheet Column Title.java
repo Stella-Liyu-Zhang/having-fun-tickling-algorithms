99eaffdeced7c3cdb085d4be417ad3bc622de9a2
Stella <l9zhang@ucsd.edu>
2023-09-17 12:00:13 -0700

    168

---
+++ Math questions/168. Excel Sheet Column Title.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while(columnNumber > 0){
+            columnNumber --;
+            sb.append((char) (columnNumber % 26 + 'A'));
+            columnNumber = (columnNumber) / 26;
+        }
+        return sb.reverse().toString();
+    }
+}
