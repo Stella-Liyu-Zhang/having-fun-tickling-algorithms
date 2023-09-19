783ba1e724170f578d09a6513e5203588d23a1f2
Stella <l9zhang@ucsd.edu>
2023-09-18 21:48:47 -0700

    88

--- Two Pointers/88. Merge Sorted Array.java
+++ Two Pointers/88. Merge Sorted Array.java
@@ -1,14 +1,25 @@
 class Solution {
+    /*
+    sorted, 
+    nums1
+    nums2
+    go from the end, ach time
+
+    */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1;
-        int j = n-1;
-        int k = m+n-1;
-        while(j >= 0){
-            if(i >= 0 && nums1[i] > nums2[j]){
-                nums1[k --] = nums1[i --];
+        int ptr1 = m-1;
+        int ptr2 = n-1;
+        int ptr = m+n-1;
+        while(ptr2 >= 0){
+            if(ptr1 >= 0 && nums1[ptr1] > nums2[ptr2]){
+                nums1[ptr] = nums1[ptr1];
+                ptr1 --;
             }else{
-                nums1[k --] = nums2[j --];
+                nums1[ptr] = nums2[ptr2];
+                ptr2 --;
             }
+
+            ptr --;
         }
     }
 }
