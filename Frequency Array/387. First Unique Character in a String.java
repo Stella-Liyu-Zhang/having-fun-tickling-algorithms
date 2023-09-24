e497f96205fbfb6d6fd6844da6495f2f61b3927e
Stella <l9zhang@ucsd.edu>
2023-09-24 18:17:04 -0400

    387

---
+++ Frequency Array/387. First Unique Character in a String.java
@@ -0,0 +1,20 @@
+class Solution {
+    /*
+    Create a freq Array of 26 length
+
+    find the first index which has 1
+
+    */
+    public int firstUniqChar(String s) {
+        int[] freqArr = new int[26];
+        for(char c: s.toCharArray()){
+            freqArr[c-'a'] ++;
+        }
+        for(int i = 0; i < s.length(); i ++){
+            if(freqArr[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
