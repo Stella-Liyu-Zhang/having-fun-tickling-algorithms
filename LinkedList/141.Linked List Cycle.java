50c3cc55807bc724180c6dab42c79e36c50dc4af
Stella <l9zhang@ucsd.edu>
2023-09-24 23:36:27 -0400

    141

--- LinkedList/141.Linked List Cycle.java
+++ LinkedList/141.Linked List Cycle.java
@@ -1,30 +1,36 @@
 /**
  * Definition for singly-linked list.
  * class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) {
- * val = x;
- * next = null;
- * }
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null)
-            return false;
-        // fast pointer
-        ListNode fast = head.next;
-        // slow pointer
-        ListNode slow = head;
+        /*
+    Solution 1: HashSet
+    put all the nodes into a hashset, and check if duplicates exist.
 
-        while (slow != fast) {
-            if (fast == null || fast.next == null) {
-                // In this case, we reaches the end (i.e. there's no such a cycle)
+    Solution 2:  Floyd's Cycle Finding Algorithm
+    If there is no cycle in the list, the fast pointer will eventually reach the end and we can return false in this case.
+    O(N)/O(N)
+
+    Now consider a cyclic list and imagine the slow and fast pointers are two runners racing around a circle track. The fast runner will eventually meet the slow runner.
+    O(N)/O(1)
+    */
+    public boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while(slow != fast){
+            if(fast == null || fast.next ==null){
                 return false;
             }
-            slow = slow.next;
             fast = fast.next.next;
+            slow = slow.next;
         }
         return true;
     }
