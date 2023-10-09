d394e09d2fc3e193cab57240526428e12eb9ae5d
Stella <l9zhang@ucsd.edu>
2023-10-08 22:52:41 -0700

    295 second attempt

--- Heap (Priority Queue)/295. Find Median from Data Stream.java
+++ Heap (Priority Queue)/295. Find Median from Data Stream.java
@@ -106,3 +106,42 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
+
+
+ class MedianFinder {
+    private PriorityQueue<Integer> large; //save large values
+    private PriorityQueue<Integer> small; //save small values
+    private boolean even = true;
+
+    public MedianFinder() {
+         large = new PriorityQueue<>();
+         small = new PriorityQueue<>(Collections.reverseOrder());
+    }
+
+    //make sure that small store the extra one in odd cases.
+    public void addNum(int num) {
+        if(even){
+            large.offer(num);
+            small.offer(large.poll());
+        }else{
+            small.offer(num);
+            large.offer(small.poll());
+        }
+        even = !even;
+    }
+    
+    public double findMedian() {
+        if(even){
+            return (small.peek() + large.peek())/2.0;
+        }else{
+            return small.peek();
+        }
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
