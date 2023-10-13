cef578234c296001587171d9087c37815891a192
Stella <l9zhang@ucsd.edu>
2023-10-13 00:35:51 -0700

    328

--- LinkedList/328. Odd Even Linked List.java
+++ LinkedList/328. Odd Even Linked List.java
@@ -51,3 +51,38 @@ class Solution {
         return head;
     }
 }
+
+//third attempt
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/*
+head tail
+oddList: head odd
+evenList: evenHead, even
+
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode odd = head, even = head.next, evenHead = head.next;
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
