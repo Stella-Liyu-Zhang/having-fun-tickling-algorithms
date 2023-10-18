0ce7de1458f4db4dc5b804f3b799c36b28259042
Stella <l9zhang@ucsd.edu>
2023-10-18 11:41:35 -0700

    392

--- Two Pointers/392. Is Subsequence.java
+++ Two Pointers/392. Is Subsequence.java
@@ -6,13 +6,12 @@ class Solution {
     space: O(1)
     */
     public boolean isSubsequence(String s, String t) {
-        int ptr1 = 0;
-        int ptr2 = 0;
+        int ptr1 = 0, ptr2 = 0;
         while(ptr1 < s.length() && ptr2 < t.length()){
-            if (s.charAt(ptr1) == t.charAt(ptr2)){
+            if(s.charAt(ptr1) == t.charAt(ptr2)){
                 ptr1 ++;
             }
-            ptr2 ++;
+            ptr2++;
         }
         return ptr1 == s.length();
     }
