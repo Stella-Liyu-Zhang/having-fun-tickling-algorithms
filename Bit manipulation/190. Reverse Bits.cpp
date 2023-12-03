2b7430446b76d6a522a801c1d23e28694c803752
Stella <l9zhang@ucsd.edu>
2023-12-03 15:06:16 -0800

    190

--- Bit manipulation/190. Reverse Bits.cpp
+++ Bit manipulation/190. Reverse Bits.cpp
@@ -1,17 +1,36 @@
 class Solution
 {
 public:
-    uint32_t reverseBits(uint32_t n)
-    {
+    /*
+    Solution 1:  Bit by bit reverse
+    Iterate from right to left, retrive the right-most bit by anding with & 1
+    For each bit, we reverse it to the correct position (i.e. (n & 1) << power). Then we accumulate this reversed bit to the final result.
+    when n == 0, there is no more bits of one left, stop the iteration
+
+    uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
-        int pos = 31;
-        while (n != 0)
-        {
-            // n = (n & 1) << pos; -> we don't need to modify the n here!!!! just add it to our ans.
-            ans += (n & 1) << pos;
-            n = n >> 1;
-            pos--;
+        int power = 31;
+
+        while(n != 0){
+            ans += (n & 1) << power;
+            n  = n >> 1;
+            power -= 1;
+
         }
         return ans;
+    }
+
+    Solution 2: divide and conquer
+
+
+    */
+    uint32_t reverseBits(uint32_t n)
+    {
+        n = (n >> 16) | (n << 16); // This line swaps the leftmost 16 bits with the rightmost 16 bits.
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
     }
 };
