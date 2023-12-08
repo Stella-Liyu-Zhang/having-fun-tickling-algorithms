920a2d928721f795005bc5ab4d9f07299857476c
Stella <l9zhang@ucsd.edu>
2023-12-07 19:56:04 -0800

    371 adding a mask

--- Bit manipulation/371. Sum of Two Integers.cpp
+++ Bit manipulation/371. Sum of Two Integers.cpp
@@ -13,10 +13,11 @@ public:
     int getSum(int a, int b)
     {
         int sum = a;
+        int mask = 0xFFFFFFFF;
         while (b != 0)
         {
-            sum = a ^ b;
-            b = (a & b) << 1;
+            sum = (a ^ b) & mask;
+            b = ((a & b) & mask) << 1;
             a = sum;
         }
         return sum;
