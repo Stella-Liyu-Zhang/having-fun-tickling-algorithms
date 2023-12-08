67bf3041fd187cb9d109372950a762bcca8161c2
Stella <l9zhang@ucsd.edu>
2023-12-07 21:24:05 -0800

    191 solution 2

--- Bit manipulation/191. Number of 1 Bits.cpp
+++ Bit manipulation/191. Number of 1 Bits.cpp
@@ -13,3 +13,23 @@ public:
         return res;
     }
 };
+
+// Solution 2
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        uint32_t res = 0;
+        ulong mask = 1;
+        for (int i = 0; i < 32; i++)
+        {
+            if ((n & mask) != 0)
+            {
+                res++;
+            }
+            mask = mask << 1;
+        }
+        return res;
+    }
+};
