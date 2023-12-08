9db2d2428775b3347321164e5cf871ec27f71d21
Stella <l9zhang@ucsd.edu>
2023-12-08 09:46:44 -0800

    287

---
+++ Bit manipulation/287. Find the Duplicate Number.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    /*
+    1. unordered_set
+    */
+    int findDuplicate(vector<int> &nums)
+    {
+        while (nums[0] != nums[nums[0]])
+        {
+            swap(nums[0], nums[nums[0]]);
+        }
+        return nums[0];
+    }
+};
