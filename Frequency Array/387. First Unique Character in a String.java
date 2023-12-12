310d79e3235ee19ee41e34897faf4d42d79f89d5
Stella <l9zhang@ucsd.edu>
2023-12-12 13:06:37 -0800

    387

--- Frequency Array/387. First Unique Character in a String.java
+++ Frequency Array/387. First Unique Character in a String.java
@@ -18,3 +18,19 @@ class Solution {
         return -1;
     }
 }
+
+//second attempt
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] charArr = new int[26];
+        for(char c: s.toCharArray()){
+            charArr[c - 'a'] ++;
+        }
+        for(int i = 0; i < s.length(); i ++){
+            if(charArr[s.charAt(i) - 'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
