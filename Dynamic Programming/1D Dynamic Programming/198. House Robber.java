93e8191b5f0150ea4156c851e6aa62f34ccd0e4a
Stella <l9zhang@ucsd.edu>
2024-01-18 10:01:59 -0800

    198

--- Dynamic Programming/1D Dynamic Programming/198. House Robber.java
+++ Dynamic Programming/1D Dynamic Programming/198. House Robber.java
@@ -18,3 +18,28 @@ class Solution {
         return dp[nums.length - 1];
     }
 }
+
+//Optimized solution of O(1) space
+class Solution {
+    /*
+    Base casese:
+    dp[0] = dp[0];
+    dp[1] = Math.max(dp[0], dp[1])
+    dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i])
+    */
+    public int rob(int[] nums) {
+        if(nums.length == 1){
+            return nums[0];
+        }
+        //int[] dp = new int[nums.length];
+        int robNextPlusOne = 0;
+        int robNext = nums[nums.length-1]; 
+
+        for(int i = nums.length - 2; i >= 0; i --){
+            int curr = Math.max(robNext, robNextPlusOne + nums[i]);
+            robNextPlusOne = robNext;
+            robNext = curr;
+        }
+        return robNext;
+    }
+}
