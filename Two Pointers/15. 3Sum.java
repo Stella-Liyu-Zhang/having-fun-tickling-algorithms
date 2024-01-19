69973e75dd40581bfa8a5c968a76357ad3973b41
Stella <l9zhang@ucsd.edu>
2024-01-19 11:20:21 -0800

    15

--- Two Pointers/15. 3Sum.java
+++ Two Pointers/15. 3Sum.java
@@ -1,43 +1,27 @@
-public /*
-Idea: [-1,0,1,2,-1,-4]
-Loop through the sorted array,
-Look at -1, check if there are a pair of ints in the rest of the array that addes up to 1, then look at 2, check if there is a pair of ints ....
-
-
-
-*/
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> ans = new HashSet<>();
+        if(nums.length == 0){
+            return new ArrayList<>(ans);
+        }
         Arrays.sort(nums);
-        List<List<Integer>> output = new LinkedList();
-        
-        for(int i = 0; i < nums.length - 2; i++){
-            if (i == 0 || i > 0 && nums[i] != nums[i-1]){
-                int sum = 0 - nums[i];
-                int left = i+1;
-                int right = nums.length - 1;
-                
-                while (left < right){
-                    if (nums[left] + nums[right] == sum){
-                        output.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                        while(left < right && nums[left] == nums[left+1]){
-                            left ++;
-                        }
-                        while(left < right && nums[right] == nums[right-1]){
-                            right --;
-                        }
-                        left ++;
-                        right --;
-                    }else if(nums[left] + nums[right] > sum){
-                        right --;
-                    }else{
-                        left ++;
-                    }
+        for(int i = 0; i < nums.length; i ++){
+            int left = i+1;
+            int right = nums.length - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left ++;
+                    right --;
+                }else if (sum <0){
+                    left ++;
+                }else{
+                    right --;
                 }
             }
         }
-        return output;
+        return new ArrayList<>(ans);
     }
-}{
-
 }
+
