4fdfde0a6f23bf70badce8fac31fe4e82c6b5944
Stella <l9zhang@ucsd.edu>
2024-01-23 10:40:57 -0800

    74

--- Binary Search/74. Search a 2D Matrix.java
+++ Binary Search/74. Search a 2D Matrix.java
@@ -1,26 +1,37 @@
 class Solution {
+    /*
+    m*n
+    O(mn)
+    O(log(mn))
+    matrix-> flattened 1d array
+    [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+    60 -> 11
+    4 -> no of cols
+    row: 2 -> 11/4
+    col : 3 -> 11 % 4
+    map each element of the 1d array into the matrix
+    1) matrix  is empty -> return false
+    2) if matrix only contains 1 element == target -> return true
+    
+    */
     public boolean searchMatrix(int[][] matrix, int target) {
-        // 1 3 5 7 3?
-        //
-
+        if(matrix.length == 0 && matrix[0].length == 0){
+            return false;
+        }
         int m = matrix.length;
         int n = matrix[0].length;
-
-        int start = 0;
-        int end = m * n - 1;
-
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            int midX = mid / n;
-            int midY = mid % n;
-
-            if (matrix[midX][midY] == target) {
+        int left = 0;
+        int right = m*n - 1;
+        while(left <= right){
+            int middle = left + (right - left)/2; //avoid overflow
+            int row = middle / n;
+            int col = middle % n;
+            if(matrix[row][col] == target){
                 return true;
-            }
-            if (matrix[midX][midY] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+            }else if (matrix[row][col] < target){
+                left = middle + 1;
+            }else{
+                right = middle - 1;
             }
         }
         return false;
