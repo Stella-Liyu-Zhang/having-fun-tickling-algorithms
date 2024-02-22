cde8f4291c95473afd39885c2251cb0c6d4f2f13
Stella <l9zhang@ucsd.edu>
2024-02-22 10:01:09 -0800

    692

--- Heap (Priority Queue)/692. Top K Frequent Words.java
+++ Heap (Priority Queue)/692. Top K Frequent Words.java
@@ -32,3 +32,33 @@ class Solution {
 
     }
 }
+
+class Solution {
+    /*
+    min heap of size k to store the largest k elements so far,
+
+    The PriorityQueue is initialized with a comparator. This comparator will determine the order in which elements are dequeued from the priority queue.
+
+    */
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> res = new ArrayList();
+        HashMap<String, Integer> map = new HashMap();
+        for(String s: words){
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        PriorityQueue<String> pq = new PriorityQueue<>(
+            (s1, s2) -> map.get(s1).equals(map.get(s2)) ? s2.compareTo(s1) : map.get(s1) - map.get(s2));
+
+        for(String s: map.keySet()){
+            pq.offer(s);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+        while(pq.isEmpty() == false){
+            res.add(pq.poll());
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
