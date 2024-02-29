a60598fd35b6b9a0914f925adf5ee6a0fef3e678
Stella <l9zhang@ucsd.edu>
2024-02-28 16:14:02 -0800

    33

--- Binary Search/33. Search in Rotated Sorted Array.java
+++ Binary Search/33. Search in Rotated Sorted Array.java
@@ -27,3 +27,41 @@ class Solution {
         return -1;
     }
 }
+
+//second attempt
+class Solution {
+    /*
+
+    binary search
+
+    pivot k can be at the start, or bottom or anywhere
+    after shifting, looks like 2 increasing subarrays
+    nums == target, return index
+    nums > target{
+        1) if nums[mid] > nums[left]
+    }
+    */
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            int midEle = nums[mid];
+            if(midEle == target){
+                return mid;
+            }else if (nums[mid] >= nums[left]){ //left is sorted 
+                if(target < nums[mid] && target >= nums[left]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{ //right is sorted 
+                if(target <= nums[right] && target > nums[mid]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
